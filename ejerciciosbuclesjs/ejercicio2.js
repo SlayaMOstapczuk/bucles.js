ae4fabe084eb9b35bc9fe31253a3aa6ae7062693
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2020-12-15 12:49:40 -0300

    hice que si -s- suma, continue el ciclo

--- ejerciciosbuclesjs/ejercicio2.js
+++ ejerciciosbuclesjs/ejercicio2.js
@@ -8,3 +8,29 @@
 10: Sobresaliente
 
 Si ingreso un numero que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el numero ingresado no es válido mostrar el mensaje “Introduce un número válido”.*/
+//Ejercicio 2
+
+let s = 1;
+
+for (let i = 1; i <= s; i++) {
+    let calificación = parseInt(prompt("Ponga su calificación: "));
+
+    if (calificación >= 0 && calificación <= 10) {
+        if (calificación <= 2) {
+            document.write("<br>Muy deficiente");
+        } else if (calificación <= 4) {
+            document.write("<br>Insuficiente");
+        } else if (calificación <= 6) {
+            document.write("<br>Sufciente");
+        } else if (calificación == 7) {
+            document.write("<br>Bien");
+        } else if (calificación <= 9) {
+            document.write("<br>Notable");
+        } else {
+            document.write("<br>Sobresaliente");
+        }
+    } else {
+        alert("Número erróneo.Introduce un número válido")
+        s++
+    }
+}
