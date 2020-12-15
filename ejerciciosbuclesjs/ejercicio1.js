7ed186430129fbe855e3ed92194a2818429ca704
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2020-12-15 13:07:17 -0300

    aprendi de mejor manera el anidado y las funcionalidades

--- ejerciciosbuclesjs/ejercicio1.js
+++ ejerciciosbuclesjs/ejercicio1.js
@@ -1,12 +1,17 @@
 /*1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.*/
 //ejercicio1
-let edad
+let s = 1
 
-for (let i = 1; i <= 1; i++) {
-    edad = parseInt(prompt("Ingrese su edad "))
-    if (edad < 18) {
-        document.write("<br>Usted no está habilitado para conducir.");
+for (let i = 1; i <= s; i++) {
+    let edad = parseInt(prompt("Ingrese su edad "))
+    if (edad >= 0 && edad <= 100) {
+        if (edad < 18) {
+            document.write("<br>Usted no está habilitado para conducir.");
+        } else {
+            document.write("<br>Usted está habilitado para conducir")
+        }
     } else {
-        document.write("<br>Usted está habilitado para conducir")
+        alert("El número no es válido. Vuelva a ingresar")
+        s++
     }
 };
