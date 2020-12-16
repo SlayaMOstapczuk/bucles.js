eb56f0aa363461f5a19ea63b6efc6f6fd26e8c0f
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2020-12-16 01:55:48 -0300

    aprendi sobre lenght y substr ademas ya entiendo mejor el ejercicio2

---
+++ ejerciciosbuclesjs/ejercicio3.js
@@ -0,0 +1,21 @@
+/*3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp*/
+
+//i++ es igual a i = (i+1)  
+
+//ejercicio 3
+
+let texto = "";
+let alerta;
+let n = 2; //variable para cortar el ciclo  (bandera)
+for (let i = 1; i < n; i++) {
+    let confirmar = confirm("concatenar cadena de texto")
+    if (confirmar == true) {
+        alerta = prompt("Ingrese cadena de texto: ");
+        texto = texto + alerta + (" - ")
+        n++
+    } else {
+        document.write(texto)
+    }
+}
